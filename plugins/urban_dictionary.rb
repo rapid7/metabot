ef88d4eb7090c3c8b6d1177d173b669ddf53de5a
Jenkins <jenkins@metasploit.com>
2012-03-09 14:24:50 -0600

    add a shorten method

--- plugins/urban_dictionary.rb
+++ plugins/urban_dictionary.rb
@@ -8,11 +8,22 @@ class UrbanDictionary
   match /urban (.+)/, method: :urban
   
   def urban(m, word)
-  	begin
-	    url = "http://www.urbandictionary.com/define.php?term=#{::CGI.escape(word)}"
-	    m.reply ::CGI.unescape(Nokogiri::HTML(open(url)).at("div.definition").text.gsub(/\s+/, ' '))
-	rescue
-		"Error: no results found"
-	end
+    begin
+      url = "http://www.urbandictionary.com/define.php?term=#{::CGI.escape(word)}"
+      m.reply shorten(::CGI.unescape(Nokogiri::HTML(open(url)).at("div.definition").text.gsub(/\s+/, ' ')))
+    rescue
+      "Error: no results found"
+    end
   end
+
+  def shorten(string, count=200)
+    if string.length >= count 
+      shortened = string[0, count]
+      splitted = shortened.split(/\s/)
+      words = splitted.length
+      string = splitted[0, words-1].join(" ") << ' ...'
+    end
+  string
+  end
+
 end
