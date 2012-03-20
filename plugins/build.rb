d437addfe71532b51e46693f2d237784f21a3878
Jonathan Cran <jcran@0x0e.org>
2012-03-20 10:44:19 -0500

    do a little housekeeping

--- plugins/build.rb
+++ plugins/build.rb
@@ -1,10 +1,20 @@
 require 'helpers'
 
 class Build
-	include Cinch::Plugin
+  include Cinch::Plugin
 
-	def initialize(*args)
-		super(*args)
-	end
+  match /latest_build/, method: :latest_build
+
+  def initialize(*args)
+    super(*args)
+  end
+  
+  #
+  # Call this method to get the latest build info - 
+  # TODO - make this non-build-specific.
+  #  
+  def latest_build(m)
+
+  end
 
 end
